ce1b8dc3715935e4404cbe7d86e2cbff1cfddf76
robertsergo <111302645+robertsergo@users.noreply.github.com>
2022-10-27 12:16:39 -0400

    API WEB et modifier imc

--- imc.js
+++ imc.js
@@ -1,6 +1,7 @@
 function calcul_metric_imc(){
     let imc = 0
     let message_erreur = "veillez remplir les champs convénablement svp".toLocaleUpperCase()
+    
     let taille = document.querySelector("#taille").value
     let poids = document.querySelector("#poids").value
     imc = (poids / (taille * taille))
@@ -20,7 +21,8 @@ function calcul_imperial_imc(){
     
     let taille = document.querySelector("#taille2").value
     let poids = document.querySelector("#poids2").value
-    imc = (poids / (taille * taille))
+    // IMC = Poids en lb / (Taille en in)² ∗ 703.07
+    imc = poids / (taille * taille) * 703.07
     if (imc <= 0 || taille <= 0 || poids <= 0 ){
         document.querySelector("#resultat2").innerHTML = message_erreur 
     }else{
@@ -29,12 +31,3 @@ function calcul_imperial_imc(){
    
      
 }
-
-
-/*
-Taille en mètre x taille en mètre = taille au carré
-Poids en kilogrammes ÷ taille au carré = IMC
-Par exemple, si vous mesurez 1.70m et que vous pesez 65kg,
- votre IMC sera : 65 ÷ (1.70 x 1.70) = 65 ÷ 2.89 = 22.49.
-
-*/
