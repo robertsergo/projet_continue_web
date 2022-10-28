ef8c81e984612bc7db6768f522f2d307f8bc1b13
robertsergo <111302645+robertsergo@users.noreply.github.com>
2022-10-27 21:15:24 -0400

    arrondir le résultat imc et colorer le message d'erreur

--- imc.js
+++ imc.js
@@ -1,11 +1,10 @@
 function calcul_metric_imc(){
-    let imc = 0
-    let message_erreur = "veillez remplir les champs convénablement svp".toLocaleUpperCase()
-    
+    let imc = 0    
+    let message_erreur = "<h2 style='color:red'>veillez remplir les champs convénablement svp.</h2>".toLocaleUpperCase(); 
     let taille = document.querySelector("#taille").value
     let poids = document.querySelector("#poids").value
     imc = (poids / (taille * taille))
-
+    imc = Math.round(imc * 100) / 100
     if (imc <= 0 || taille <= 0 || poids <= 0 ){
         document.querySelector("#resultat1").innerHTML = message_erreur 
     }else{
@@ -17,12 +16,11 @@ function calcul_metric_imc(){
 
 function calcul_imperial_imc(){ 
     let imc = 0 
-    let message_erreur = "veillez remplir les champs convénablement svp".toLocaleUpperCase()
-    
+    let message_erreur = "<h2 style='color:red'>veillez remplir les champs convénablement svp.</h2>".toLocaleUpperCase();     
     let taille = document.querySelector("#taille2").value
-    let poids = document.querySelector("#poids2").value
-    // IMC = Poids en lb / (Taille en in)² ∗ 703.07
+    let poids = document.querySelector("#poids2").value    
     imc = poids / (taille * taille) * 703.07
+    imc = Math.round(imc * 100) / 100
     if (imc <= 0 || taille <= 0 || poids <= 0 ){
         document.querySelector("#resultat2").innerHTML = message_erreur 
     }else{
--- readme.txt
+++ readme.txt
@@ -2,6 +2,9 @@ nom du site: Projet continu
 date: octobre 2022
 auteur: Robert-Sergo Dimanche
 
+
+Description & mode d'utilisation:
+
 Le site est l'organisation de quelques exercice de devoir en html, javascript et css. Il compremd
 5 pages: une page d'accueil, une page html, une page css et une page api web.
 
@@ -25,5 +28,15 @@ La page api.html utilise "fetch" pour afficher des photos avec leurs identités.
 que je retrouve sur le tutoriel du site suivant:
 
  source code de API WEB: https://www.digitalocean.com/community/tutorials/js-fetch-api
+
+ license:
+ Ce projet est open source.
+ Toute personne qui désire utiliser ce code pour l'améliorer ou pour l'utilisation éducative 
+ est bienvenu.
+
+ resource importée:
+ La page api.html est un code importé, tiré du site:
+ https://www.digitalocean.com/community/tutorials/js-fetch-api
+
 
 
